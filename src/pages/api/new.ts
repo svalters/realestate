7e96cd5254902cd89b3764539719d5a10834ab89
Valters <stukens.valters@gmail.com>
2023-07-25 19:01:15 +0300

    add key phrase

--- src/pages/api/new.ts
+++ src/pages/api/new.ts
@@ -3,6 +3,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { start } from "@/api/scrape";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  start();
-  res.status(200).json({ started: true });
+  const { secret } = req.query;
+
+  const secretMatch = (process.env.SCRAPER_SECRET || undefined) === secret;
+  if (secretMatch) {
+    start();
+  }
+  return res.status(200).json({ started: secretMatch });
 }
