63d6f662100261491003789af9194c7b2fddcc3b
Valters <stukens.valters@gmail.com>
2023-08-13 22:30:45 +0300

    await

--- src/pages/api/new.ts
+++ src/pages/api/new.ts
@@ -2,12 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { start } from "@/api/scrape";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { secret } = req.query;
 
   const secretMatch = (process.env.SCRAPER_SECRET || undefined) === secret;
   if (secretMatch) {
-    start();
+    await start();
   }
   return res.status(200).json({ started: secretMatch });
 }
